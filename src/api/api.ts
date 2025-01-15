00dfd5823c2ad9453c62b2ce761871edb77f1160
Aditya <adityakumarjha.276@gmail.com>
2025-01-15 22:59:15 +0530

    fix some minor changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = import.meta.env.VITE_API_BASE_URL;
 
 interface ShortenUrlResponse {
   shortUrl: string;
